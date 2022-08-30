3badce60f4281517280a7931af2ab15296e5ba0d
Sebastián Salazar Molina <sebasalazar@gmail.com>
2022-08-29 23:40:44 -0400

    Ajustes del mensaje

--- app/Monomio.h
+++ app/Monomio.h
@@ -1,18 +1,71 @@
 #ifndef MONOMIO_H
 #define MONOMIO_H
 
+/**
+ * @brief Clase que representa un monomio algebraico.
+ *
+ */
 class Monomio {
 public:
-    Monomio();
-    Monomio(const Monomio& orig);
-    ~Monomio();
-    double GetCoeficiente() const;
-    void SetCoeficiente(double coeficiente);
-    int GetGrado() const;
-    void SetGrado(int grado);
+  /**
+   * @brief Constructor de un objeto Monomio.
+   *
+   */
+  Monomio();
+
+  /**
+   * @brief Constructor de un objeto Monomio.
+   *
+   * @param orig Objeto original.
+   */
+  Monomio(const Monomio &orig);
+
+  /**
+   * @brief Destructor de un objeto Monomio
+   *
+   */
+  ~Monomio();
+
+  /**
+   * @brief Getter del Coeficiente
+   *
+   * @return double
+   */
+  double GetCoeficiente() const;
+
+  /**
+   * @brief Setter del Coeficiente
+   *
+   * @param coeficiente
+   */
+  void SetCoeficiente(double coeficiente);
+
+  /**
+   * @brief Getter del Grado
+   *
+   * @return int
+   */
+  int GetGrado() const;
+
+  /**
+   * @brief Setter del Grado
+   *
+   * @param grado
+   */
+  void SetGrado(int grado);
+
 private:
-    double coeficiente;
-    int grado;
+  /**
+   * @brief Coeficiente del monomio.
+   *
+   */
+  double coeficiente;
+
+  /**
+   * @brief Grado del monomio.
+   *
+   */
+  int grado;
 };
 
 #endif /* MONOMIO_H */
--- app/Utils.h
+++ app/Utils.h
@@ -1,57 +1,55 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
+#include "Monomio.h"
 #include <algorithm>
 #include <cmath>
+#include <cstring>
+#include <iostream>
 #include <regex>
-#include "Monomio.h"
+#include <string>
+#include <vector>
 
 /**
  * Espacio de nombres para utilidades de manejo de monomios/polinomios
  */
 namespace Utils {
 
+/**
+ *
+ * @param texto cadena de texto
+ * @return String sin espacios
+ */
+std::string acomodar(std::string texto);
 
+/**
+ *
+ * @param polinomioStr representación de texto del polinomio
+ * @return Un arreglo con los terminos separados.
+ */
+std::vector<std::string> obtener_terminos(std::string polinomioStr);
 
-    /**
-     * 
-     * @param texto cadena de texto
-     * @return String sin espacios
-     */
-    std::string acomodar(std::string texto);
+/**
+ *
+ * @param texto cadena de texto
+ * @return Objeto monomio
+ */
+Monomio str2Monomio(std::string texto);
 
-    /**
-     * 
-     * @param polinomioStr representación de texto del polinomio
-     * @return Un arreglo con los terminos separados.
-     */
-    std::vector<std::string> obtener_terminos(std::string polinomioStr);
+/**
+ *
+ * @param polinomioStr Representación de string del polinomio
+ * @return arreglo del polinomio.
+ */
+std::vector<Monomio> convertir(std::string polinomioStr);
 
-    /**
-     * 
-     * @param texto cadena de texto
-     * @return Objeto monomio
-     */
-    Monomio str2Monomio(std::string texto);
-
-    /**
-     * 
-     * @param polinomioStr Representación de string del polinomio
-     * @return arreglo del polinomio.
-     */
-    std::vector<Monomio> convertir(std::string polinomioStr);
-
-    /**
-     * 
-     * @param polinomio Arreglo de polinomios
-     * @param value Elemento a evaluar
-     * @return el valor resultante
-     */
-    double evaluar(std::vector<Monomio> polinomio, double value);
-}
+/**
+ *
+ * @param polinomio Arreglo de polinomios
+ * @param value Elemento a evaluar
+ * @return el valor resultante
+ */
+double evaluar(std::vector<Monomio> polinomio, double value);
+} // namespace Utils
 
 #endif /* UTILS_H */
